d2c13d69f468a70f8d78cb7b4664d0e316dba19c
Euller Braz <euller.braz99@gmail.com>
2022-02-19 22:13:12 -0300

    Fixed mongoose connect on server

--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 const {
   PORT = 3000,
   DB_URL = 'mongodb://localhost:27017/pro-franchising-challenge',
 } = process.env;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = require('./app');
 
