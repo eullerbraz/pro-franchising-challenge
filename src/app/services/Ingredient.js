c0ca0923e3ecb96d7e573466d12ce1140c0f5c55
Euller Braz <euller.braz99@gmail.com>
2022-02-19 21:27:15 -0300

    Created ingredient service

---
+++ src/app/services/Ingredient.js
@@ -0,0 +1,67 @@
+const Joi = require('joi');
+
+const IngredientModel = require('../models/Ingredient');
+
+const validateIngredient = (ingredient) => Joi.object({
+  name: Joi.string().not().empty().required(),
+  measure: Joi.string().not().empty().required(),
+  unitPrice: Joi.number().required(),
+}).validate(ingredient);
+
+const getAll = async () => {
+  const ingredients = await IngredientModel.find({});
+
+  return ingredients;
+}
+
+const create = async (ingredient) => {
+  const { error } = validateIngredient(ingredient);
+
+  if (error) {
+    const [{ message }] = error.details;
+    return { message, code: 400 };
+  }
+
+  const created = await IngredientModel.create(ingredient);
+
+  return created
+}
+
+const findById = async (id) => {
+  const found = await IngredientModel.findByIdAndUpdate(id);
+
+  if (!found) return { message: 'Invalid id', code: 400 }
+
+  return found 
+}
+
+const update = async (id, ingredient) => {
+  const { error } = validateIngredient({ ...ingredient, createDate: new Date()});
+
+  if (error) {
+    const [{ message }] = error.details;
+    return { message, code: 400 };
+  }
+
+  const updated = await IngredientModel.findByIdAndUpdate(id, ingredient, { new: true });
+
+  if (!updated) return { message: 'Invalid id', code: 400 }
+
+  return updated 
+}
+
+const remove = async (id) => {
+  const deleted = await IngredientModel.findByIdAndDelete(id);
+
+  if (!deleted) return { message: 'Invalid id', code: 400 }
+
+  return deleted
+}
+
+module.exports = {
+  getAll,
+  create,
+  findById,
+  update,
+  remove,
+}
