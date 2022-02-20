aff177db56403f0037be64ec9701738ba901fc74
Euller Braz <euller.braz99@gmail.com>
2022-02-19 21:52:02 -0300

    Add ingredient router to app

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { json } = require('body-parser');
 const cors = require('cors');
 
+const IngredientRouter = require('./app/routers/Ingredient');
+
 const app = express();
 
 app.use(cors());
 
 app.use(json());
 
-app.get('/', (req, res) => {
-  return res.status(200).json({ respone: 'ok' })
-});
+app.use('/ingredient', IngredientRouter)
 
 module.exports = app;
