db087319f2da4a8d5fcfa8ce1e425c938a631c8c
Euller Braz <euller.braz99@gmail.com>
2022-02-19 21:52:26 -0300

    Add mongoose connection to server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
-const { PORT = 3000 } = process.env;
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const {
+  PORT = 3000,
+  DB_URL = 'mongodb://localhost:27017/pro-franchising-challenge',
+} = process.env;
 
 const app = require('./app');
 
