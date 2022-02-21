d85bdc56b6203be43263f682a3b018e418ddd73f
Euller Braz <euller.braz99@gmail.com>
2022-02-21 10:27:36 -0300

    Created product controller

---
+++ src/app/controllers/Product.js
@@ -0,0 +1,50 @@
+const ProductService = require('../services/Product');
+
+const getAll = async (_req, res) => {
+  const products = await ProductService.getAll();
+
+  return res.status(200).json(products);
+}
+
+const create = async (req, res, next) => {
+  const {code, message, product} = await ProductService.create(req.body);
+
+  if (message) return next({ message, code });
+
+  return res.status(201).json(product);
+}
+
+const findById = async (req, res, next) => {
+  const { id } = req.params;
+  const {code, message, product} = await ProductService.findById(id, req.body);
+
+  if (message) return next({ message, code });
+
+  return res.status(200).json(product);
+}
+
+const update = async (req, res, next) => {
+  const { id } = req.params;
+  const {code, message, product} = await ProductService.update(id, req.body);
+
+  if (message) return next({ message, code });
+
+  return res.status(200).json(product);
+}
+
+const remove = async (req, res, next) => {
+  const { id } = req.params;
+  const { product, message, code } = await ProductService.remove(id);
+
+  if (message) return next({ message, code });
+
+  return res.status(200).json(product);
+}
+
+module.exports = {
+  getAll,
+  create,
+  findById,
+  update,
+  remove,
+}
